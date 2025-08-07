eec74d0f2cff3f2ee6d0a38796a505d92035ade1
iqbaladiatma <gazamuhammadadiatmav@gmail.com>
2025-08-07 10:18:00 +0700

    update

---
+++ lib/bab3/belajar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BelajarPage extends StatefulWidget {
+  const BelajarPage({super.key});
+
+  @override
+  State<BelajarPage> createState() => _BelajarPageState();
+}
+
+class _BelajarPageState extends State<BelajarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Belajar Flutter BAB 3')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [Text('Belajar Flutter')],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bab3/belajar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,49 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage(title: 'Contoh Stateful'));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Kamu telah menekan tombol ini:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Klik untuk menambah',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'BelajarPage',
+      routes: {'BelajarPage': (context) => const BelajarPage()},
     );
   }
 }
