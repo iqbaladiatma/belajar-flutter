0ddfc510482d22707c6424e9157bd75ecd205ee9
maman2008 <abdurrahmanjkt2@gmail.com>
2025-08-13 07:35:01 +0700

    update gambar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_biodata/bab3/belajar.dart';
 import 'bab4/belajar.dart';
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: 'KalkulatorPage',
-      routes: {'KalkulatorPage': (context) => const KalkulatorPage()},
+      routes: {'KalkulatorPage': (context) => const BelajarPage()},
     );
   }
 }
